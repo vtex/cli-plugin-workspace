3954d0c891c5ee9368612723cdc71cca5aa4d645
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2021-02-25 12:25:27 -0300

    Update delete.ts

--- src/commands/workspace/delete.ts
+++ src/commands/workspace/delete.ts
@@ -4,13 +4,21 @@ import workspaceDelete from '../../modules/delete'
 import { CustomCommand, ColorifyConstants } from 'vtex'
 
 export default class WorkspaceDelete extends CustomCommand {
-  static description = `Deletes one or many ${ColorifyConstants.ID('workspaces')} from the current ${ColorifyConstants.ID('account')}.`
+  static description = `Deletes one or many ${ColorifyConstants.ID(
+    'workspaces'
+  )} from the current ${ColorifyConstants.ID('account')}.`
 
-  static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex workspace delete')} workspaceName`, `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex workspace delete')} workspaceName1 workspaceName2`]
+  static examples = [
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex workspace delete')} workspaceName`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex workspace delete')} workspaceName1 workspaceName2`,
+  ]
 
   static flags = {
     ...CustomCommand.globalFlags,
-    force: oclifFlags.boolean({ char: 'f', description: `Deletes the specified ${ColorifyConstants.ID('workspace')} even if it is currently in use.` }),
+    force: oclifFlags.boolean({
+      char: 'f',
+      description: `Deletes the specified ${ColorifyConstants.ID('workspace')} even if it is currently in use.`,
+    }),
     yes: oclifFlags.boolean({ char: 'y', description: 'Answers yes to all prompts.' }),
   }
 
@@ -18,7 +26,12 @@ export default class WorkspaceDelete extends CustomCommand {
 
   static args = [
     { name: 'workspace1', required: true, description: `Name of the ${ColorifyConstants.ID('workspace')} to delete.` },
-    { name: 'ithWorkspace', required: false, multiple: true, description: `Name of the multiple ${ColorifyConstants.ID('workspaces')} to delete.` },
+    {
+      name: 'ithWorkspace',
+      required: false,
+      multiple: true,
+      description: `Name of the multiple ${ColorifyConstants.ID('workspaces')} to delete.`,
+    },
   ]
 
   async run() {
